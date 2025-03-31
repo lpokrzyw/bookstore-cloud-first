5ef18a83b9bc2c9c5f9515bba1cc167a0cc3baf5
LukpoDevs <71985109+LukpoDevs@users.noreply.github.com>
2025-03-31 19:29:20 +0200

    Login
    Login created

---
+++ books/src/main/java/com/ecommerce/books/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.ecommerce.books.config;
+
+import com.ecommerce.books.users.CustomUserDetailsService;
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout").permitAll())
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.disable());
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ books/src/main/java/com/ecommerce/books/users/UserRepository.java
@@ -0,0 +1,6 @@
+package com.ecommerce.books.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository extends JpaRepository<User, String>{
+    User findByUsername(String username);
+}
